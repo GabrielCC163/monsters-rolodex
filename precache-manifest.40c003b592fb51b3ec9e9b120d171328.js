92c66d996bfdce4974eb78f7d788404e74b1afba
Gabriel Brum <gabriel.brum@dotgroup.com.br>
2019-12-25 17:35:06 -0300

    Updates

---
+++ precache-manifest.40c003b592fb51b3ec9e9b120d171328.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b46afbcc835bc18e0e97ab48978f5a01",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "6ab01798c693772ce904",
+    "url": "/monsters-rolodex/static/css/main.94b05e7e.chunk.css"
+  },
+  {
+    "revision": "dcd2657b3ef8f78d14f4",
+    "url": "/monsters-rolodex/static/js/2.e6fbc2c9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.e6fbc2c9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6ab01798c693772ce904",
+    "url": "/monsters-rolodex/static/js/main.b77d8ae7.chunk.js"
+  },
+  {
+    "revision": "6e191d8baec223c78143",
+    "url": "/monsters-rolodex/static/js/runtime-main.c1ee7b67.js"
+  }
+]);
